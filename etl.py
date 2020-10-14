3e5169817d87cb92c7cce619eda01afbb791db9d
Guillermo <guillermo.garcia.espuela@gmail.com>
2020-10-14 17:05:17 +0200

    Commented functions of python files

--- etl.py
+++ etl.py
@@ -4,12 +4,26 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    This function load the staging tables from the S3 buckets using the COPY function created in "sql_queries.py".
+
+    INPUTS:
+    * cur: the cursor variable
+    * conn: the connection to the Redshift cluster.
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+    This function process and inserts the data from the staging tables to the fact and dimensional tables using the queries in "sql_queries.py".
+
+    INPUTS:
+    * cur: the cursor variable
+    * conn: the connection to the Redshift cluster.
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
